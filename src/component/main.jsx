3430ca1e5a464d38688575b749ebfd31da5636c5
daryashcherbinina <ludmila670502@gmail.com>
2023-04-02 01:14:44 +0700

    разметка и стили

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './style/main.css'
+import {IoMdUndo, IoMdRedo}  from 'react-icons/io'
+import {GrRotateLeft, GrRotateRight} from 'react-icons/gr'
 const Main = () => {
-    let Element = [
-        { name: 'gfgg'}, { name: 'gfgfg'}, { name: 'gfg'}, { name: 'gfgfg'}
+    let Filter = [
+        { name: 'Насыщенность'}, { name: 'Сепия'}, { name: 'Яркость'}, { name: 'Контраст'}
     ]
     return (
       <div className = "Editor">
@@ -11,14 +13,42 @@ const Main = () => {
                 <h2>Фоторедактор</h2>
             </div>
             <div className="body">
-                <div className="leftSide">
+            <div className="leftSide">
+                <div className="wrap">
                     <div className="filters">
-                        <span>Filters</span>
+                        <span>Фильтры</span>
                         <div className="filterElements">
                             {
-                                Element.map((v,i) => <button key = {i}>{v.name}</button>)
+                                Filter.map((v,i) => <button key = {i}>{v.name}</button>)
                             }
                         </div>
+    
+                    </div>
+                    <div className="rotate">
+                        <label htmlFor="">Повернуть</label>
+                        <div className="rotateElements"> 
+                        <button><GrRotateLeft/></button>
+                        <button><GrRotateRight/></button>
+                        </div>
+                    </div>
+                  </div>  
+
+                </div>
+                
+                <div className="imageSection">
+                    <div className="image">
+                        <label htmlFor="choose">
+                            Выбрать фото
+                        </label>
+                    </div>
+                    <div className="imageSelect">
+                        <button className='undo'><IoMdUndo/></button>
+                        <button className='redo'><IoMdRedo/></button>
+                        <button>Обновить</button>
+                        <button>Сохранить</button>
+                        <label htmlFor = "choose" className = "custom">
+                        </label>
+                        <input type="file" id='choose' />
                     </div>
                 </div>
             </div>
